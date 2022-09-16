00b2325e70b8fe85cad82dec9820718eaa434802
Alexandra <alexandraneff@live.com>
2022-09-15 19:15:17 -0500

    commit tree c14f7a139048bd720f1933c916ecc64475bb27a9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function createEmployeeRecord(employee) {
+
+return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+}}
+
+function createEmployeeRecords(employee) {
+return employee.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(record, dateStamp) {
+    const splitDateStamp = dateStamp.split(' ')
+    
+            record.timeInEvents.push({
+            type: "TimeIn", 
+            hour: parseInt(splitDateStamp[1], 10),
+            date: splitDateStamp[0]
+            })
+            return record
+    }  
+
+function createTimeOutEvent(record, dateStamp) {
+    const splitDateStamp = dateStamp.split(' ')
+    record.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(splitDateStamp[1], 10),
+        date: splitDateStamp[0]
+    })
+    return record
+}
